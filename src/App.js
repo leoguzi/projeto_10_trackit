13c69c24a628380436a4a1168ba7b684d311ba9d
leoguzi <leoguzi@hotmail.com>
2021-09-10 12:55:56 -0300

    prepared the enviroment for starting the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./components/login/Login";
+import SignUp from "./components/signup/SignUp";
+import Habits from "./components/habits/Habits";
+import Today from "./components/today/Today";
+import History from "./components/history/History";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/cadastro">
+          <SignUp />
+        </Route>
+        <Route path="/habitos">
+          <Habits />
+        </Route>
+        <Route path="/hoje">
+          <Today />
+        </Route>
+        <Route path="/historico">
+          <History />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
